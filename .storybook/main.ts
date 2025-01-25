183076648524c383a4b6aff2edd554731001498f
jiyoon <ljy10499@gmail.com>
2025-01-25 21:16:27 +0900

    docs: storybook 업그레이드

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
@@ -9,9 +10,16 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     '@storybook/addon-postcss',
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
