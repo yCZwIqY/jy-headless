5d55e747c1669832cbd58b3fb6c0def00326ee93
jiyoon <ljy10499@gmail.com>
2025-04-12 12:16:59 +0900

    [FIX] build 구조 변경

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { default as Button } from './buttons';
-export { default as Input } from './inputs';
-export { default as Dropdown } from './selectors';
+export * from './buttons';
+export * from './inputs';
+export * from './selectors';
 export * from './types';
 export * from './utils';
 export * from './hooks';
--- src/selectors/Dropdown/DropdownDefaultTrigger.tsx
+++
@@ -1,5 +0,0 @@
-const DropdownDefaultTrigger = ({ value }: { value: boolean }) => {
-  return <span style={{ cursor: 'pointer' }}>{value ? '▼' : '▲'}</span>;
-};
-
-export default DropdownDefaultTrigger;
