87dee678cafddfe0b7d4d71bfa0138900a3e9e2e
jylee <jylee@saycore.io>
2026-01-30 14:24:31 +0900

    feat: 카테고리 변경

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
-import Select from '../Dropdown/Select';
+import Select from '../Select/Select';
 
 const meta: Meta<typeof Select> = {
   title: 'Components/Select',
